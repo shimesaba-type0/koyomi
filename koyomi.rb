cc1dd3f54cfe7293391157924bcc895ea7cac1fa
shimesaba-type0 <t.k.shimesaba@gmail.com>
2016-12-13 22:03:01 +0900

    全年月日取ってきてるようだ。

--- koyomi.rb
+++ koyomi.rb
@@ -1,5 +1,4 @@
 # coding: utf-8
-#
 
 require 'open-uri'
 require 'csv'
@@ -7,79 +6,62 @@ require 'nokogiri'
 
 master_url = 'https://moonlabo.com/cgi-bin/cln/cln_clm.cgi?OpDv=usd'
 
-# urls = [1910, 1920, 1930, 1940, 1950, 1960, 1970, \
-#         1980, 1990, 2000, 2010, 2020, 2040]
+urls = ["1910", "1920", "1930", "1940", "1950", "1960", "1970", \
+        "1980", "1990", "2000", "2010", "2020", "2040"]
 
-url = master_url + '1910'
-charset = nil
-html = open(url) do |f|
-  charset = f.charset
-  f.read
-end
-
-tbody = Nokogiri::HTML.parse(html, nil, charset)
-# p tbody
+csv = ""
 month_count = 0
-tbody.xpath('//*[@id="col_main"]/article[2]/div/table/tbody').each do |node|
-  #puts node.search('tr').text
-  current_month = ""
-  node.search("tr").each do |tr|
-    #puts tr.search("td").text
-    tds = tr.search("td")
+urls.each do |url_year|
+  # スクレイピング先の文字コードとHTMLの取得
+  url = master_url + url_year
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset
+    f.read
+  end
 
-    # this is test part ###############
-    #puts "length = #{tds.length}"
-    #puts "tds[1] = #{tds[1].text}"
-    #tds.each do |m|
-    #  puts m.text
-    #end
-    ####################################
+  # 取得したHTMLをパースしてDOM化
+  tbody = Nokogiri::HTML.parse(html, nil, charset)
 
-    if tds.length == 9 or tds.length == 10 then
-      #puts "length = #{tds.length}"
-      #puts "current_month = #{current_month}"
-      #puts "tds[1] = #{tds[1].text}"
-      if tds[1].text != current_month then
-        puts "-------------------------------------"
-        puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}"
-        puts "-------------------------------------"
-    #    #puts tds.map |m| m.text
-    #    csv << tds.map |m| m.text
-        #datalists = CSV.open()
-        #  datalists.puts [tds[0].text, tds[1].text, tds[2].text]
-        #datalists.close
-        month_count = month_count + 1
+  # 必要なデータを取得し、CSV化
+  tbody.xpath('//*[@id="col_main"]/article[2]/div/table/tbody').each do |node|
+    #puts node.search('tr').text
+    current_month = ""
+    node.search(".//tr").each do |tr|
+      arr = Array.new()
+      tds = tr.search(".//td")
+
+      # 本当にlength が9と10だけ？違ったらabort
+      if tds.length == 9 or tds.length == 10 then
+        #puts "length = #{tds.length}"
+        #puts "current_month = #{current_month}"
+        #puts "tds[1] = #{tds[1].text}"
+        if tds[1].text != current_month then
+          puts "-------------------------------------"
+          puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}"
+          puts "-------------------------------------"
+          #arr.push(tds[0].text,tds[1].text,tds[2].text)
+          arr.push(tds.text)
+          csv << arr.to_csv
+
+          month_count = month_count + 1
+        end
+      else
+        puts "error record is: #{tds.text}"
+        abort
       end
-    else
-      abort
+      current_month = tds[1].text
     end
-    current_month = tds[1].text
   end
 end
 
-puts month_count
+# Output
+puts "how many days? = #{month_count}"
+
+File.open('export.csv', 'w') do |io|
+  io.write csv
+end
 
 
-# html = url
-# tbody = Nokogiri::HTML.parse(html, '//*[@id="col_main"]/article[2]/div/table/tbody')
-# puts tbody
 
-
-# urls.each do | url_base |
-#   url = master_url + url_base.to_s
-#   html = url.fetch
-#   tbody = Nokogiri::HTML.parse(html, '//*[@id="col_main"]/article[2]/div/table/tbody')
-#   current_month = ""
-#   puts tbody
-#   #tbody.search("/tr").each | tr | do
-#   #  tds = tr.search("/td")
-#   #  if tds.length == 9 or tds.length == 10
-#   #    if tds[1]! == current_month
-#   #      csv << tds.map |m| m.value
-#   #    end
-#   #  else
-#   #    abort
-#   #  end
-#   #end
-# end
 
