14c643024e35562c92efb8bd84d9fd135150e129
shimesaba-type0 <t.k.shimesaba@gmail.com>
2016-12-14 05:55:25 +0900

    added loop bad...

--- koyomi.rb
+++ koyomi.rb
@@ -6,8 +6,9 @@ require 'nokogiri'
 
 master_url = 'https://moonlabo.com/cgi-bin/cln/cln_clm.cgi?OpDv=usd'
 
-urls = ["1910", "1920", "1930", "1940", "1950", "1960", "1970", \
-        "1980", "1990", "2000", "2010", "2020", "2040"]
+urls = ["1910"]
+#urls = ["1910", "1920", "1930", "1940", "1950", "1960", "1970", \
+#        "1980", "1990", "2000", "2010", "2020", "2040"]
 
 csv = ""
 month_count = 0
@@ -38,9 +39,16 @@ urls.each do |url_year|
         #puts "tds[1] = #{tds[1].text}"
         if tds[1].text != current_month then
           puts "-------------------------------------"
-          puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}"
+          puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}.#{tds[5].text}"
           puts "-------------------------------------"
           #arr.push(tds[0].text,tds[1].text,tds[2].text)
+          #for i in 0..tds.length do
+          #  if i == 0 then
+          #    arr.push(tds[i].text)
+          #  else
+          #    arr.push(",#{tds[i].text}")
+          #  end
+          #end
           arr.push(tds.text)
           csv << arr.to_csv
 
