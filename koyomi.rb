8904d5f7191bdbfdc2e2fa677a07dea153dc534a
shimesaba-type0 <t.k.shimesaba@gmail.com>
2016-12-15 05:57:50 +0900

    大体出来た？

--- koyomi.rb
+++ koyomi.rb
@@ -6,9 +6,9 @@ require 'nokogiri'
 
 master_url = 'https://moonlabo.com/cgi-bin/cln/cln_clm.cgi?OpDv=usd'
 
-urls = ["1910"]
-#urls = ["1910", "1920", "1930", "1940", "1950", "1960", "1970", \
-#        "1980", "1990", "2000", "2010", "2020", "2040"]
+#urls = ["1910"]
+urls = ["1910", "1920", "1930", "1940", "1950", "1960", "1970", \
+        "1980", "1990", "2000", "2010", "2020", "2040"]
 
 csv = ""
 month_count = 0
@@ -38,18 +38,17 @@ urls.each do |url_year|
         #puts "current_month = #{current_month}"
         #puts "tds[1] = #{tds[1].text}"
         if tds[1].text != current_month then
-          puts "-------------------------------------"
-          puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}.#{tds[5].text}"
-          puts "-------------------------------------"
+          #puts "-------------------------------------"
+          #puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}.#{tds[5].text}"
+          puts tds[2].text
+          #puts "-------------------------------------"
           #arr.push(tds[0].text,tds[1].text,tds[2].text)
+          #
+          # if you want all column then comment out below 3 lines.
           #for i in 0..tds.length do
-          #  if i == 0 then
-          #    arr.push(tds[i].text)
-          #  else
-          #    arr.push(",#{tds[i].text}")
-          #  end
+          #    arr.push(tds[i])
           #end
-          arr.push(tds.text)
+          arr.push(tds[2].text)
           csv << arr.to_csv
 
           month_count = month_count + 1
@@ -69,7 +68,6 @@ puts "how many days? = #{month_count}"
 File.open('export.csv', 'w') do |io|
   io.write csv
 end
-
 
 
 
