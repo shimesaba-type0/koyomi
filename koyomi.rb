31d5b1640867bc913a94d4223ee4b07fe7cd22f4
shimesaba-type0 <t.k.shimesaba@gmail.com>
2016-12-18 22:14:37 +0900

    update: 不要な行を削除し説明を追加。多分これで終わり

--- koyomi.rb
+++ koyomi.rb
@@ -10,8 +10,12 @@ master_url = 'https://moonlabo.com/cgi-bin/cln/cln_clm.cgi?OpDv=usd'
 urls = ["1910", "1920", "1930", "1940", "1950", "1960", "1970", \
         "1980", "1990", "2000", "2010", "2020", "2040"]
 
+# initializing 
 csv = ""
 word = "月節"
+arr = Array.new()
+
+# 10年単位でページが作られているので、そのページ分ループ
 urls.each do |url_year|
   # スクレイピング先の文字コードとHTMLの取得
   url = master_url + url_year
@@ -26,23 +30,16 @@ urls.each do |url_year|
 
   # 必要なデータを取得し、CSV化
   tbody.xpath('//*[@id="col_main"]/article[2]/div/table/tbody').each do |node|
-    #puts node.search('tr').text
+    # tr をserachしてtrの分だけループ
     node.search(".//tr").each do |tr|
-      arr = Array.new()
       tds = tr.search(".//td")
-
       # 本当にlength が9と10だけ？違ったらabort
       if tds.length == 9 or tds.length == 10 then
-        #puts "length = #{tds.length}"
-        #puts "current_month = #{current_month}"
-        #puts "tds[1] = #{tds[1].text}"
+        # tds[5].text の中に word の文字列が含まれているか。
+        # 月節が含まれていれば、その行は節入り日を表している。
         if tds[5].text.include?(word) then
-        #if tds[1].text != current_month then
-          #puts "-------------------------------------"
-          #puts "#{tds[0].text}.#{tds[1].text}.#{tds[2].text}.#{tds[5].text}"
-          #puts tds[2].text
-          #puts "-------------------------------------"
-          #arr.push(tds[0].text,tds[1].text,tds[2].text)
+          # arr に節入り日を入れて、CSVにわざわざしている。
+          # 理由はまだない。
           arr.push(tds[2].text)
           csv << arr.to_csv
         end
@@ -50,13 +47,14 @@ urls.each do |url_year|
         puts "error record is: #{tds.text}"
         abort
       end
-
     end
   end
+  # 進捗を表すために付けた。
   puts "#{url_year} / 2040"
 end
 
 
+# Export file (File type is CSV)
 File.open('export.csv', 'w') do |io|
   io.write csv
   puts "export.csv file was made"
